5db3c518483d9b6c511f98fd8b7abde42c4d6a9c
Koji Takaki <pr5r1uqr@s.okayama-u.ac.jp>
2022-12-29 11:12:43 +0900

    deleted test file

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
-from tkinter import filedialog
+from matplotlib import pyplot
+
+import GetIndices
+import ReadFile
+import StartEnd
 
 '''
-コマンドラインベースで動かす
-・まず読み込むファイルをGUIで選んでもらう
-・次にFSECとTryptophanを選択してもらう
-・plotして図をアウトプットする(file名を記入してもらう)
+まず複数のファイルを重ね合わせるかどうか聞く
+仮に１つのファイルのみの場合はaskopenfilename, ２つの時はfileがタプルなので注意する
+今のところ読み込むファイルはFSECファイルのtxt形式のみ
+一行ずつリストとしてdataListに格納して正規表現で目的の場所の最初と最後のインデックスをとる
+その範囲においてdataListを読み込み空白で区切って一番目をtime, ２番目をintensityとしてリストに格納する
+matplotlibによって作図する
 '''
+print("This is plotting program for FSEC")
 
+# ファイルの読み込みとListへの格納
+rf = ReadFile.ReadFile()
+dataList = rf.readfile()
 
-def inputFile():
-    typ = [("textfile", "*.txt"), ("csvfile", "*.csv"), ("Excelfile", "*.xlsx")]
-    fle = filedialog.askopenfilenames(filetypes=typ)
-    print(fle)
+# 目的の列のインデックスを得る
+getIndex = GetIndices.GetIndices()
 
+# indexをしらべる
+gfpStart = getIndex.getGFPindex(dataList)
+gfpEnd = getIndex.getGFPindexend(dataList)
+trypStart = getIndex.getTrypindex(gfpEnd)
+trypEnd = getIndex.getTrypindexend(dataList)
 
-def main():
-    print("This is plotting program for FSEC")
-    inputFile()
+startEnd = StartEnd.StartEnd()
+sel = startEnd.getSel()
+start = startEnd.getStart(sel, gfpStart, trypStart)
+end = startEnd.getEnd(sel, gfpEnd, trypEnd)
+
+i = 0
+time = []
+intensity = []
+
+for l in dataList[start:end]:
+    line = l.split()
+    if len(line) < 1:
+        break
+    time.append(float(line[0]))
+    intensity.append(float(line[1]))
+
+print(time[-1])
+print("Input the title")
+print("図のタイトルを記入してください")
+title = input(">>")
+
+pyplot.title(title)
+pyplot.xlabel("Ret.time(min)")
+pyplot.ylabel("GFP fluorescence intensity (A.U.)")
+pyplot.plot(time, intensity)
+
+pyplot.show()
