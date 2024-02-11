eecc34651265eb0731f976bd98756b3f11d26163
toasty-kj <cozytakaki@gmail.com>
2024-02-11 12:59:48 +0900

    [change] routing moduleの設定

--- projects/renderer/src/app/home-page/file-uploader/file-uploader.component.ts
+++ projects/renderer/src/app/home-page/file-uploader/file-uploader.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-file-uploader',
   templateUrl: './file-uploader.component.html',
   styleUrls: ['./file-uploader.component.css'],
 })
-export class FileUploaderComponent {}
+export class FileUploaderComponent implements OnInit {
+  users: { userId: number; userName: string }[] = []
+  currentTime = new Date()
+
+  async ngOnInit() {
+    this.users = await window.myAPI.loadUsers()
+  }
+}
