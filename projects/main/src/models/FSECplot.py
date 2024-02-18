546737fe6b513c0515fca89e367447a4b81d3fb0
toasty-kj <pr5r1uqr@s.okayama-u.ac.jp>
2024-02-18 13:16:41 +0900

    [add] とりあえず画面から作図できるようにする

--- projects/main/src/models/FSECplot.py
+++ projects/main/src/models/FSECplot.py
@@ -6,7 +6,8 @@ class FSECplot:
     def plotfsec(self, lists, sel, zoom):
         print("Input the title")
         print("図のタイトルを記入してください")
-        title = input("ENTER to skip >>")
+        # title = input("ENTER to skip >>")
+        title = 'test title'
 
         pyplot.title(title)
         if sel == "1":
@@ -21,7 +22,8 @@ class FSECplot:
             intensity = lists[i].get_intensity(lists[i])
             print("図に表示する以下のファイルの名前を教えてください。")
             print("what is the legend of ", lists[i].name)
-            legend = input("ENTER to default >> " + lists[i].default)
+            # legend = input("ENTER to default >> " + lists[i].default)
+            legend =  lists[i].default
             if legend == "":
                 legend = lists[i].default
             pyplot.plot(time, intensity, label=legend)
